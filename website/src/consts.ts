727481f8f59fbe512f23712946aa2b3222ee6187
Shashwat Amin <shash@Shashs-Macbook-Air-M2.local>
2025-03-24 17:38:18 +1100

    Fixed a mysql bug and testing render.yaml

--- website/src/consts.ts
+++ website/src/consts.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Operational.co - Event tracker tool for tech products";
-export const SITE_DESCRIPTION = "Operational.co - Event tracker tool for tech products";
+export const SITE_TITLE = "Operational.co - Open-source Event tracker tool for tech products";
+export const SITE_DESCRIPTION =
+	"Operational.co - Open-source Event tracker tool for tech products. Track critical events and get notified in real-time.";
